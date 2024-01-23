f2673e04cea580a5da86076a060e25010353879c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-23 18:25:02 +0000

    feat: redirect `/` to `/text/transform`

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/")
+    return NextResponse.redirect(new URL("/text/transform", request.url));
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
