02b4c9d8c7d13516e5a662ff3bbf66a0bdc45b04
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-24 20:49:11 -0300

    feat(navbar): small improvements

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client'
 
-import { ReactNode } from "react";
+import { ReactNode } from 'react'
 import {
   ArrowLeftRight,
   AsteriskSquare,
@@ -9,94 +9,94 @@ import {
   KeyRound,
   Text,
   Wand2,
-} from "lucide-react";
+} from 'lucide-react'
 
-import { Separator } from "~/shared/components/separator";
-import { NavbarItem } from "./navbar-item";
+import { Separator } from '~/shared/components/separator'
+import { NavbarItem } from './navbar-item'
 
 const Section = ({
   title,
   children,
 }: {
-  title: string;
-  children: ReactNode;
+  title: string
+  children: ReactNode
 }) => (
-  <div className="space-y-1">
-    <span className="px-3 text-sm opacity-70">{title}</span>
-    <div>{children}</div>
+  <div className='space-y-1'>
+    <span className='px-3 text-sm opacity-70'>{title}</span>
+    <div className='space-y-px'>{children}</div>
   </div>
-);
+)
 
 type Props = {
-  isOnDrawer?: boolean;
-};
+  isOnDrawer?: boolean
+}
 export function Navbar({ isOnDrawer = false }: Props) {
   return (
-    <div className="space-y-4 text-lg p-7 md:p-0">
-      <Section title="Text">
+    <div className='space-y-4 text-lg p-7 md:p-0'>
+      <Section title='Text'>
         <NavbarItem
           isOnDrawer={isOnDrawer}
           Icon={Text}
-          title="Transform"
-          path="/text/transform"
+          title='Transform'
+          path='/text/transform'
         />
 
         <NavbarItem
           isOnDrawer={isOnDrawer}
           disabled
           Icon={Wand2}
-          title="Styling"
-          path="/text/styling"
+          title='Styling'
+          path='/text/styling'
         />
 
         <NavbarItem
           isOnDrawer={isOnDrawer}
           disabled
           Icon={BarChart3}
-          title="Statistics"
-          path="/text/statistics"
+          title='Statistics'
+          path='/text/statistics'
         />
       </Section>
 
       <Separator />
 
-      <Section title="Cryptography">
+      <Section title='Cryptography'>
         <NavbarItem
           isOnDrawer={isOnDrawer}
           disabled
           Icon={AsteriskSquare}
-          title="Ciphers"
-          path="/cryptography/ciphers"
+          title='Ciphers'
+          path='/cryptography/ciphers'
         />
 
         <NavbarItem
           isOnDrawer={isOnDrawer}
           disabled
           Icon={Fingerprint}
-          title="Hash functions"
-          path="/cryptography/hash"
+          title='Hash functions'
+          path='/cryptography/hash'
         />
       </Section>
 
       <Separator />
 
-      <Section title="Misc">
+      <Section title='Misc'>
         <NavbarItem
           isOnDrawer={isOnDrawer}
           disabled
           Icon={ArrowLeftRight}
-          title="Converter"
-          path="/misc/converter"
+          title='Converter'
+          path='/misc/converter'
         />
 
         <NavbarItem
           isOnDrawer={isOnDrawer}
           disabled
           Icon={KeyRound}
-          title="Password Generator"
-          path="/misc/password"
+          title='Password Generator'
+          path='/misc/password'
         />
       </Section>
     </div>
-  );
+  )
 }
