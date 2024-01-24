08a2513099280dec2447b149f7203c2475c07ead
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-24 13:11:47 +0000

    feat(theme-toggle): create `ThemeToggle` button

---
+++ src/app/_components/theme-toggle.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "~/shared/components/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/shared/components/dropdown-menu";
+
+const TriggerButton = () => (
+  <DropdownMenuTrigger asChild>
+    <Button variant="outline" size="icon" className="bg-transparent">
+      <Sun
+        size="1em"
+        strokeWidth={1.5}
+        className="text-xl rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      />
+      <Moon
+        size="1em"
+        className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  </DropdownMenuTrigger>
+);
+
+export function ThemeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <TriggerButton />
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
