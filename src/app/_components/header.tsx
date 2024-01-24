1aeb8e383bc8b5c13c7e7678811fa94c2a3cea7d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-24 13:31:42 +0000

    feat(header): add theme toggle button and update colors

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,5 +1,8 @@
+import { ThemeToggle } from "./theme-toggle";
+
 export const Header = () => (
-  <header className="w-full flex items-end px-2">
-    <h1 className="text-3xl">Text Tools</h1>
+  <header className="w-full flex items-center justify-between p-2 bg-primary-foreground rounded-lg">
+    <h1 className="text-xl text-primary">Text Tools</h1>
+    <ThemeToggle />
   </header>
 );
