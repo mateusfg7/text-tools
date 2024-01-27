777eaa573cbad013fe9afc453e5432a916e9253f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-26 23:50:40 -0300

    fix(pad-zero): format negative numbers

--- src/shared/lib/pad-zero.ts
+++ src/shared/lib/pad-zero.ts
@@ -1,1 +1,5 @@
-export const padZero = (n: number) => (n < 10 ? `0${n}` : n)
+export const padZero = (n: number) => {
+  if (!(n < 10 && n > -10)) return String(n)
+
+  return n < 0 ? `-0${Math.abs(n)}` : `0${n}`
+}
