937cd198182877057292e086b510c12be5cf928d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-04-30 23:25:23 +0000

    chore(devcontainer): add Rust setup step on `post-create.sh` script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Initial update and installs
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl
+
 # Setup ZSH
 mv ~/.zshrc ~/.zshrc.bak
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" -- \
@@ -20,3 +33,15 @@ git config --global alias.rollback "reset --soft HEAD~1"
 # Install BUN
 curl -fsSL https://bun.sh/install | bash
 echo '\n# bun\nexport BUN_INSTALL="$HOME/.bun"\nexport PATH=$BUN_INSTALL/bin:$PATH' >> ~/.zshrc
+
+
+# Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
+
+cargo install cargo-expand
+cargo install cargo-edit
